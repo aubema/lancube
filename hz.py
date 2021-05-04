d8be2900c2a04d6b9ae5216b1ada0a9cf9118824
Élysé Lapalme <ely.lapalme@gmail.com>
2021-05-04 13:53:48 +0000

    temp

--- hz.py
+++ hz.py
@@ -1,8 +1,15 @@
 import csv
 import pandas as pd
 import time
+from datetime import datetime
 
 while True:
+    today = str(datetime.now())
+    hour = int(today[11:13])
+    minute = int(today[14:16])
+    second = float(today[17:])
+    times = hour*3600 + minute*60 + second
+
     data = pd.read_csv("/var/www/html/data/gps.txt")
     data_list = data.values.tolist()
 
@@ -14,8 +21,8 @@ while True:
     lon_t_moins = data_list[-2][2]
 
 
-    futur_lat = (lat_t-lat_t_moins)/(t-tmoins)
-    futur_lon = (lon_t-lon_t_moins)/(t-tmoins)
+    futur_lat = (lat_t-lat_t_moins)/(t-tmoins)*(times-t)+lat_t
+    futur_lon = (lon_t-lon_t_moins)/(t-tmoins)*(times-t)+lon_t
 
     print(futur_lat)
     print(futur_lon)
