061eeaa837e14b93d2d10b8264200bb43cabdb5f
Julien-Pierre Houle <jphoule29@hotmail.com>
2021-08-21 10:46:34 -0400

    Update lancube_inventory.py

--- lancube_inventory.py
+++ lancube_inventory.py
@@ -13,6 +13,7 @@ from datetime import datetime
 from pyproj import CRS, Transformer
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+from scipy.ndimage.interpolation import shift
 
 
 # PARAMETERS
@@ -20,10 +21,12 @@ PATH_DATAS = "Lan3_mtl"
 light_folder = 'Lights/' # path to the lights folder
 street_distance = 5 # Maximal distance(m) between coord and street
 h = 2   # height of the lancube in meters
+K = 1  # K factor
 
 
 
 # Load Lights tech clouds
+print('Loading lights tech cloud')
 list_tech = []
 frames = []
 
@@ -167,31 +170,39 @@ df3 = df[df['Sensor'] == 'S3']
 df5 = df[df['Sensor'] == 'S5']
 df1 = df1.drop(0)
 
-dfS = pd.DataFrame({'Values':    (df3['Value'].values - df5['Value'].values),
+dfS = pd.DataFrame({'Value':    (df3['Value'].values - df5['Value'].values),
                     'Distance':   df3['Traveled Distance'].values,
-                    'Init_index': df3.index.values})
-
-# Moving Average
-df1['MA'] = df1["Value"].rolling(window=3, center=True).mean()
-dfS['MA'] = dfS['Values'].rolling(window=3, center=True).mean()
-
+                    'BC_3': (df3['Blue']  / df3['Clear']).values,
+                    'RC_3': (df3['Red']   / df3['Clear']).values,
+                    'GC_3': (df3['Green'] / df3['Clear']).values,
+                    'BC_5': (df5['Blue']  / df5['Clear']).values,
+                    'RC_5': (df5['Red']   / df5['Clear']).values,
+                    'GC_5': (df5['Green'] / df5['Clear']).values})
 
 S1_value = df1['Value'].values
 S1_dist  = df1['Traveled Distance'].values
 
+# Moving Average
+df1['MA'] = df1["Value"].rolling(window=3, center=True).mean()
+dfS['MA'] = dfS['Value'].rolling(window=3, center=True).mean()
 
-# Find index peaks S1 & S3-S5 (side)
+
+
+
+# ------------ Find index peaks S1 & S3-S5 (side) ------------
+
+print('Finding peaks and primes..')
 idx_EV1, _m = find_peaks(df1['MA'], height=0.04, prominence=0.02)
 idx_EVS, _m = find_peaks(abs(dfS['MA']), height=0.04, prominence=0.02)
 
 # S1 & S3-S5 peaks & distance values
 EV1, ED1 = S1_value[idx_EV1], S1_dist[idx_EV1]
-EVS, EDS = dfS['Values'].iloc[idx_EVS], dfS['Distance'].iloc[idx_EVS].values
+EVS, EDS = dfS['Value'].iloc[idx_EVS], dfS['Distance'].iloc[idx_EVS].values
 
 
-idx_simul_S1   = []
+idx_simul   = []
 idx_simul_side = []
-index_S1_only  = []
+idx_S1  = []
 
 # Check for (S3-S5) peak close to S1 peak
 for pos in idx_EV1:
@@ -200,98 +211,142 @@ for pos in idx_EV1:
     simul_dist = EDS[((d-3) < EDS) & (EDS < (d+3))] # EDS in a 5m interval from S1 peak
 
     if len(simul_dist) == 1:  # high light fixture
-        idx_simul_S1.append(pos)
+        idx_simul.append(pos)
         idx_simul_side.append(dfS['Distance'].tolist().index(simul_dist[0]))
 
     if len(simul_dist) == 0: # peak only in S1
-        index_S1_only.append(pos)
+        idx_S1.append(pos)
 
 
 # Get index low lights (peak only in S3-S5)
-idx_low_lights = [elem for elem in idx_EVS if elem not in idx_simul_side]
+idx_S35 = [elem for elem in idx_EVS if elem not in idx_simul_side]
 
 
-idx_left = []
-idx_right = []
 
-# Find fraction values close to each peak
-for index, pos in enumerate(idx_simul_S1):
+def find_prime(index_peaks, values, dist):
+    """ Find prime value +- 2 points close to the index peak.
+        Will take the left value if available else the right one. """
 
-    if S1_value[pos-2] < S1_value[pos] and (S1_dist[pos]-S1_dist[pos-2]) < 15:
-        idx_left.append(pos-2)
-    else:
-        idx_left.append(np.nan)
+    idx = np.array(index_peaks)
+    mask_L = (values[idx-2] < values[idx]) & (dist[idx]-dist[idx-2] < 15)
+    mask_R = (values[idx+2] < values[idx]) & (dist[idx+2]-dist[idx] < 15)
+    idx_left  = np.where(mask_L, idx, np.nan) -2
+    idx_right = np.where(mask_R, idx, np.nan) +2
 
-    if S1_value[pos+2] < S1_value[pos] and (S1_dist[pos+2]-S1_dist[pos]) < 15:
-        idx_right.append(pos+2)
-    else:
-        idx_right.append(np.nan)
+    # Take left value if availaible else take right
+    nan_min = np.nanmin([idx_left, idx_right], axis=0)
+    mask = ~np.isnan(nan_min)
 
+    idx_prime = nan_min[mask].astype(int)
+    idx_peaks = np.array(index_peaks)[mask].astype(int)
 
-# Take left value if availaible else take right
-nan_min = np.nanmin([idx_left, idx_right], axis=0)
-mask = ~np.isnan(nan_min)
-idx_prime = nan_min[mask].astype(int)
-idx_peaks = np.array(idx_simul_S1)[mask].astype(int)
+    return idx_prime, idx_peaks, mask
 
-
-# Get simultanous peaks values
-EV1, ED1 = S1_value[idx_peaks], S1_dist[idx_peaks]
-EVp, EDp = S1_value[idx_prime], S1_dist[idx_prime]
-EVS, EDS = dfS['Values'].iloc[idx_peaks].values, dfS['Distance'].iloc[idx_peaks].values
+# Calling find prime function to get index
+prime_simul, peaks_simul, deleting = find_prime(idx_simul, S1_value, S1_dist)
+prime_S1,    peaks_S1,    _del     = find_prime(idx_S1, S1_value, S1_dist)
+prime_S35,   peaks_S35,   _del     = find_prime(idx_S35, dfS['Value'].values, dfS['Distance'].values)
 
 
-# # Equation 14: Horizontal distance between Lan3 and lights fixture at t.
-D = ED1 - EDp # distance between EV1 and EV1p
-d = D / ( (EV1/EVp)**(2/3) * ( ((EV1**2)/ (EVS)**2)+1 ) - ((EV1**2)/ (EVS)**2)-1)
+# Get peaks and prime values
+EV, ED = S1_value[peaks_simul], S1_dist[peaks_simul]
+EVp, EDp = S1_value[prime_simul], S1_dist[prime_simul]
+EV_side  = dfS['Value'].iloc[np.array(idx_simul_side)[deleting]].values
+
+EV1, ED1 = S1_value[peaks_S1], S1_dist[peaks_S1]
+EV1p, ED1p = S1_value[prime_S1], S1_dist[prime_S1]
+
+EV35, ED35 = dfS['Value'].iloc[peaks_S35].values, dfS['Distance'].iloc[peaks_S35].values
+EV35p, ED35p = dfS['Value'].iloc[prime_S35].values, dfS['Distance'].iloc[prime_S35].values
 
 
-# Equation 15: Light fixture height.
-H = d*(EV1/abs(EVS))-h
 
 
-# Equation 16: Illuminance normal line
-dist_light = np.sqrt((H - h)**2 + d**2) # distance between lan3 and light
-E_illum = (EV1 * dist_light) / (H - h)
+# ------------ Getting lights characteristics ------------
+print('Getting lights characteristics..')
+
+# Distance between peaks and prime
+D_simul = ED - EDp
+D_S1    = ED1 - ED1p
+D_S35   = ED35 - ED35p
+
+# Horizontal distance between Lan3 and lights fixture at t.
+d_simul = D_simul / ( (EV/EVp)**(2/3) * ( ((EV**2)/ (EV_side)**2)+1 ) - ((EV**2)/ (EV_side)**2)-1)
+d_S1    = np.full(len(D_S1), 0)
+d_S35   = D_S35 * (EV35p/EV35)**(1/3) / np.sqrt(1 - (EV35p/EV35)**(2/3))
+
+# Light fixture height.
+H_simul = d_simul * (EV/abs(EV_side))-h
+H_S1    =  (D_S1 * ((EV1p/EV1)**(1/3)) / np.sqrt(1 - ((EV1p/EV1)**(2/3))) ) + h
+H_S35   = np.full(len(D_S35), h)
+
+# Concatenate scenarios data
+D = np.concatenate([D_simul, D_S1, D_S35])
+d = np.concatenate([d_simul, d_S1, d_S35])
+H = np.concatenate([H_simul, H_S1, H_S35])
+E_val = np.concatenate([EV, EV1, EV35])
+
+# Equation 16
+E_perpendicular = E_val * (np.sqrt((H - h)**2 + d**2)) / (H - h)
 
 
-# RC, GC, BC = np.zeros(len(dfS)), np.zeros(len(dfS)), np.zeros(len(dfS))
-# index_S3 = dfS[dfS['Values'] > 0].index.values
-# index_S5 = dfS['Init_index'][dfS['Values'] < 0].values
-# df3['Blue'].iloc[index_S3]
 
-# Find nearest point Lights technologie cloud
-BC = (df1['Blue']  / df1['Clear']).iloc[idx_peaks].values
-GC = (df1['Green'] / df1['Clear']).iloc[idx_peaks].values
-RC = (df1['Red']   / df1['Clear']).iloc[idx_peaks].values
-M = np.stack((BC, GC, RC), axis=-1)
+# Find nearest point in the lights technologie cloud
+peak_simul_S1 = np.concatenate([peaks_simul, peaks_S1])
+M_simul_S1 = np.stack( ((df['Blue']  / df['Clear']).iloc[peak_simul_S1].values,
+                        (df['Green'] / df['Clear']).iloc[peak_simul_S1].values,
+                        (df['Red']   / df['Clear']).iloc[peak_simul_S1].values), axis=-1)
+
+
+S35_peaks_val = dfS['Value'].iloc[peaks_S35]
+idx_S3 = S35_peaks_val[S35_peaks_val > 0].index.values
+idx_S5 = S35_peaks_val[S35_peaks_val < 0].index.values
+
+# Convatening both dataframe (S3 & S5) while maintening index order
+M_S35 = np.stack((
+        pd.concat([dfS['BC_3'].iloc[idx_S3], dfS['BC_5'].iloc[idx_S5]], sort=False).sort_index().values,
+        pd.concat([dfS['GC_3'].iloc[idx_S3], dfS['GC_5'].iloc[idx_S5]], sort=False).sort_index().values,
+        pd.concat([dfS['RC_3'].iloc[idx_S3], dfS['RC_5'].iloc[idx_S5]], sort=False).sort_index().values),
+        axis=-1)
+
+# Combine the 2D arrays
+M = np.concatenate((M_simul_S1, M_S35), axis=0)
 
 distance = np.sum((M[:,:,None] - lights_cloud) **2, 1)
 idx_closest = np.argmin(distance, 1)
-light_tech  = np.array(list_tech)[idx_closest] # return closest light technologie
+lights_tech  = np.array(list_tech)[idx_closest] # closest light technologie
+
+
+# Estimating the light flux
+uplight_dict = {'HPS': 0.5,
+                'MH' : 0.5,
+                'LED': 0.0}
+
+U = np.array([uplight_dict[t] for t in lights_tech])
+
+flux = (2*np.pi*K*E_perpendicular) * (d**2 + (H-h)**2) / (1 - U)
 
 
 
-# Writing data
-data_peaks = {'lat'   : df1['Latitude'].iloc[idx_peaks].tolist(),
-              'lon'   : df1['Longitude'].iloc[idx_peaks].tolist(),
-              'D'     : list(D),
-              'd'     : list(d),
-              'H'     : list(H),
-              'technologie': list(light_tech)}
+# ----------------- Writing data -----------------
+print('Writing data...')
 
-data_S1 = {'lat'  : df1['Latitude'].iloc[index_S1_only].tolist(),
-          'lon'   : df1['Longitude'].iloc[index_S1_only].tolist()}
+lats = df1['Latitude'].iloc[peak_simul_S1].tolist() + \
+       df3['Latitude'].iloc[peaks_S35].tolist()
 
-data_side = {'lat'  : df1['Latitude'].iloc[idx_low_lights].tolist(),
-             'lon'  : df1['Longitude'].iloc[idx_low_lights].tolist()}
+lons = df1['Longitude'].iloc[peak_simul_S1].tolist() + \
+       df3['Longitude'].iloc[peaks_S35].tolist()
 
-df_peaks = pd.DataFrame.from_dict(data_peaks)
-df_S1    = pd.DataFrame.from_dict(data_S1)
-df_side  = pd.DataFrame.from_dict(data_side)
+df_invent = pd.DataFrame({
+            'lat'   : lats,
+            'lon'   : lons,
+            'D'     : list(np.concatenate([D_simul, D_S1, D_S35])),
+            'd'     : list(np.concatenate([d_simul, d_S1, d_S35])),
+            'H'     : list(np.concatenate([H_simul, H_S1, H_S35])),
+            'tech'  : list(lights_tech),
+            'E_perp': list(E_perpendicular),
+            'flux'  : list(flux)} )
 
-
-df_invent = pd.concat([df_peaks, df_S1, df_side]).reset_index(drop=True)
 df_invent['h'] = h
 # df.to_csv('lan3_inventory.csv', index=False, sep=' ')
 print('\nDone.')
@@ -307,23 +362,9 @@ plt.plot(dfS['Distance'], dfS['Values'], label='S3-S5')
 plt.plot(df1['Traveled Distance'], df1['MA'], label='S1 MA')
 plt.plot(dfS['Distance'], dfS['MA'], label='Side MA')
 
-
 # PEAKS
 plt.plot(ED1, EV1, 'o', c='red')
 plt.plot(EDp, EVp, 'o', c='pink') # Prime
-
-
-# plt.plot(df1['Traveled Distance'].iloc[idx_EV1], df1['Value'].iloc[idx_EV1], 'o')
-# plt.plot(dfS['Distance'].iloc[idx_EVS], dfS['Values'].iloc[idx_EVS], 'o')
-
-
-# Matched
-# plt.plot(ED1, EV1, 'x', c='r')
-# plt.plot(EDS, EVS, 'x', c='r')
-
-# Not Matched
-# plt.plot(S1_dist[index_S1_only], S1_value[index_S1_only], 'x', c='b', label='S1 only')
-# plt.plot(side_d[idx_low_lights], side_v[idx_low_lights], 'x', c='g',  label='Side only')
 
 plt.legend()
 plt.show()
