921a6f5bf953e9dbe08579455429203cf0f11d8f
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-10-13 14:38:11 -0400

    web preview script

--- web-preview.bash
+++ web-preview.bash
@@ -8,29 +8,29 @@ echo "<html>" > /var/www/html/index.html
 echo "<body>" >> /var/www/html/index.html
 echo "DATE:" $y"-"$mo"-"$d "<br>">> /var/www/html/index.html
 echo "Time:" $hour":"$min":"$sec  "<br>">> /var/www/html/index.html
-echo"<table>" >> /var/www/html/index.html
-echo"  <tr>" >> /var/www/html/index.html
-echo"    <th>Gain</th>" >> /var/www/html/index.html
-echo"    <th>Integ. Time</th>" >> /var/www/html/index.html
-echo"    <th>CCT</th>" >> /var/www/html/index.html
-echo"    <th>Lux</th>" >> /var/www/html/index.html
-echo"    <th>R</th>" >> /var/www/html/index.html
-echo"    <th>G</th>" >> /var/www/html/index.html
-echo"    <th>B</th>" >> /var/www/html/index.html
-echo"    <th>C</th>" >> /var/www/html/index.html
-echo"    <th>Status</th>" >> /var/www/html/index.html
-echo"  </tr>" >> /var/www/html/index.html
-echo"  <tr>" >> /var/www/html/index.html
-echo"    <th>"$ga"</th>" >> /var/www/html/index.html
-echo"    <th>"$acqt"</th>" >> /var/www/html/index.html
-echo"    <th>"$temp"</th>" >> /var/www/html/index.html
-echo"    <th>"$lux"</th>" >> /var/www/html/index.html
-echo"    <th>"$r"</th>" >> /var/www/html/index.html
-echo"    <th>"$g"</th>" >> /var/www/html/index.html
-echo"    <th>"$b"</th>" >> /var/www/html/index.html
-echo"    <th>"$c"</th>" >> /var/www/html/index.html
-echo"    <th>"$tail"</th>" >> /var/www/html/index.html
-echo"  </tr>" >> /var/www/html/index.html
-echo"</table>" >> /var/www/html/index.html
+echo "<table>" >> /var/www/html/index.html
+echo "  <tr>" >> /var/www/html/index.html
+echo "    <th>Gain</th>" >> /var/www/html/index.html
+echo "    <th>Integ. Time</th>" >> /var/www/html/index.html
+echo "    <th>CCT</th>" >> /var/www/html/index.html
+echo "    <th>Lux</th>" >> /var/www/html/index.html
+echo "    <th>R</th>" >> /var/www/html/index.html
+echo "    <th>G</th>" >> /var/www/html/index.html
+echo "    <th>B</th>" >> /var/www/html/index.html
+echo "    <th>C</th>" >> /var/www/html/index.html
+echo "    <th>Status</th>" >> /var/www/html/index.html
+echo "  </tr>" >> /var/www/html/index.html
+echo "  <tr>" >> /var/www/html/index.html
+echo "    <th>"$ga"</th>" >> /var/www/html/index.html
+echo "    <th>"$acqt"</th>" >> /var/www/html/index.html
+echo "    <th>"$temp"</th>" >> /var/www/html/index.html
+echo "    <th>"$lux"</th>" >> /var/www/html/index.html
+echo "    <th>"$r"</th>" >> /var/www/html/index.html
+echo "    <th>"$g"</th>" >> /var/www/html/index.html
+echo "    <th>"$b"</th>" >> /var/www/html/index.html
+echo "    <th>"$c"</th>" >> /var/www/html/index.html
+echo "    <th>"$tail"</th>" >> /var/www/html/index.html
+echo "  </tr>" >> /var/www/html/index.html
+echo "</table>" >> /var/www/html/index.html
 echo "</body>" >> /var/www/html/index.html
 echo "</html>" >> /var/www/html/index.html
