364056f130ba153c5894c0d8de709594da46ab21
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-10-13 15:59:48 -0400

    web preview script

--- web-preview.bash
+++ web-preview.bash
@@ -2,36 +2,44 @@
 y=`date +%Y`
 mo=`date +%m`
 d=`date +%d`
-grep "S1"  /var/www/html/data/$y-$mo-$d.csv | tail -1 | sed 's/,/ /g' > toto.tmp
-read bidon year month day hour min sec  lat lon alt nSat ga acqt temp lux r g b c tail bidon < toto.tmp
-echo "<html>" > /var/www/html/index.html
-echo "<body>" >> /var/www/html/index.html
-echo "DATE:" $y"-"$mo"-"$d "<br>">> /var/www/html/index.html
-echo "Time:" $hour":"$min":"$sec  "<br>">> /var/www/html/index.html
-echo "<br>">> /var/www/html/index.html
-echo "<table border=1>" >> /var/www/html/index.html
-echo "  <tr>" >> /var/www/html/index.html
-echo "    <th>Gain</th>" >> /var/www/html/index.html
-echo "    <th>Integ. Time</th>" >> /var/www/html/index.html
-echo "    <th>CCT</th>" >> /var/www/html/index.html
-echo "    <th>Lux</th>" >> /var/www/html/index.html
-echo "    <th>R</th>" >> /var/www/html/index.html
-echo "    <th>G</th>" >> /var/www/html/index.html
-echo "    <th>B</th>" >> /var/www/html/index.html
-echo "    <th>C</th>" >> /var/www/html/index.html
-echo "    <th>Status</th>" >> /var/www/html/index.html
-echo "  </tr>" >> /var/www/html/index.html
-echo "  <tr>" >> /var/www/html/index.html
-echo "    <th>"$ga"</th>" >> /var/www/html/index.html
-echo "    <th>"$acqt"</th>" >> /var/www/html/index.html
-echo "    <th>"$temp"</th>" >> /var/www/html/index.html
-echo "    <th>"$lux"</th>" >> /var/www/html/index.html
-echo "    <th>"$r"</th>" >> /var/www/html/index.html
-echo "    <th>"$g"</th>" >> /var/www/html/index.html
-echo "    <th>"$b"</th>" >> /var/www/html/index.html
-echo "    <th>"$c"</th>" >> /var/www/html/index.html
-echo "    <th>"$tail"</th>" >> /var/www/html/index.html
-echo "  </tr>" >> /var/www/html/index.html
-echo "</table>" >> /var/www/html/index.html
-echo "</body>" >> /var/www/html/index.html
-echo "</html>" >> /var/www/html/index.html
+i=0
+while [ $i -lt 12 ]
+do let i=i+1
+	grep "S1"  /var/www/html/data/$y-$mo-$d.csv | tail -1 | sed 's/,/ /g' > toto.tmp
+	read bidon year month day hour min sec  lat lon alt nSat ga acqt temp lux r g b c tail bidon < toto.tmp
+	echo "<html>" > /var/www/html/index.html
+	echo "<head>" > /var/www/html/index.html
+	echo "  <meta http-equiv="refresh" content="10">" >> /var/www/html/index.html
+	echo "</head>" >> /var/www/html/index.html
+	echo "<body>" >> /var/www/html/index.html
+	echo "DATE:" $y"-"$mo"-"$d "<br>">> /var/www/html/index.html
+	echo "Time:" $hour":"$min":"$sec  "<br>">> /var/www/html/index.html
+	echo "<br>">> /var/www/html/index.html
+	echo "<table border=1>" >> /var/www/html/index.html
+	echo "  <tr>" >> /var/www/html/index.html
+	echo "    <th>Gain</th>" >> /var/www/html/index.html
+	echo "    <th>Integ. Time</th>" >> /var/www/html/index.html
+	echo "    <th>CCT</th>" >> /var/www/html/index.html
+	echo "    <th>Lux</th>" >> /var/www/html/index.html
+	echo "    <th>R</th>" >> /var/www/html/index.html
+	echo "    <th>G</th>" >> /var/www/html/index.html
+	echo "    <th>B</th>" >> /var/www/html/index.html
+	echo "    <th>C</th>" >> /var/www/html/index.html
+	echo "    <th>Status</th>" >> /var/www/html/index.html
+	echo "  </tr>" >> /var/www/html/index.html
+	echo "  <tr>" >> /var/www/html/index.html
+	echo "    <th>"$ga"</th>" >> /var/www/html/index.html
+	echo "    <th>"$acqt"</th>" >> /var/www/html/index.html
+	echo "    <th>"$temp"</th>" >> /var/www/html/index.html
+	echo "    <th>"$lux"</th>" >> /var/www/html/index.html
+	echo "    <th>"$r"</th>" >> /var/www/html/index.html
+	echo "    <th>"$g"</th>" >> /var/www/html/index.html
+	echo "    <th>"$b"</th>" >> /var/www/html/index.html
+	echo "    <th>"$c"</th>" >> /var/www/html/index.html
+	echo "    <th>"$tail"</th>" >> /var/www/html/index.html
+	echo "  </tr>" >> /var/www/html/index.html
+	echo "</table>" >> /var/www/html/index.html
+	echo "</body>" >> /var/www/html/index.html
+	echo "</html>" >> /var/www/html/index.html
+	sleep 5
+done
