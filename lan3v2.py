d7a869b3d33d5610ef5a0d808900c974036a50cf
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-12-15 18:37:04 -0500

    test gain

--- lan3v2.py
+++ lan3v2.py
@@ -43,16 +43,37 @@ TCS34725_REG_ENABLE_AEN = 0x02  # ADC Enable
 TCS34725_REG_ENABLE_PON = 0x01  # Power ON
 
 # TCS34725 Time Register Configuration
-TCS34725_REG_ATIME_2_4 = 0xFF  # Atime = 2.4 ms, Cycles = 1
-TCS34725_REG_ATIME_9_6 = 0xFC  # Atime = 9.6 ms, Cycles = 4
-TCS34725_REG_ATIME_38_4 = 0xF0  # Atime = 38.4 ms, Cycles = 16
-TCS34725_REG_ATIME_153_6 = 0xC0  # Atime = 153.6 ms, Cycles = 64
-TCS34725_REG_ATIME_614_4 = 0x00  # Atime = 614.4 ms, Cycles = 256
-TCS34725_REG_WTIME_4_8 = 0xFE  # Wtime = 4.8 ms, Cycles 2
-TCS34725_REG_WTIME_12 = 0xFB  # Wtime = 12 ms, Cycles 5
-TCS34725_REG_WTIME_40_8 = 0xEF  # Wtime = 40.8 ms, Cycles 17
-TCS34725_REG_WTIME_156 = 0xD3  # Wtime = 156 ms , Cycles 65
-TCS34725_REG_WTIME_614_4 = 0x00  # Wtime = 614.4 ms Cycles 256
+TCS34725_REG_TIME_1 = 0xFF  # Atime = 2.4 ms, Cycles = 1
+TCS34725_REG_TIME_2 = 0xFE  # Atime = 4.8 ms, Cycles = 2
+TCS34725_REG_TIME_4 = 0xFC  # Atime = 9.6 ms, Cycles = 4
+TCS34725_REG_TIME_8 = 0xF8  # Atime = 19.2 ms, Cycles = 8
+TCS34725_REG_TIME_16 = 0xF0  # Atime = 38.4 ms, Cycles = 16
+TCS34725_REG_TIME_32 = 0xE0  # Atime = 76.8 ms, Cycles = 32
+TCS34725_REG_TIME_64 = 0xC0  # Atime = 153.6 ms, Cycles = 64
+TCS34725_REG_TIME_128 = 0x80  # Atime = 307.2 ms, Cycles = 128
+TCS34725_REG_TIME_256 = 0x00  # Atime = 614.4 ms, Cycles = 256
+
+TIME_1 = 2.4
+TIME_2 = 4.8
+TIME_4 = 9.6
+TIME_8 = 19.2
+TIME_16 = 38.4
+TIME_32 = 76.8
+TIME_64 = 153.6
+TIME_128 = 307.2
+TIME_256 = 614.4
+
+# saturation is related to the number of cycles
+# SAT= cycle * 1024
+SAT_1 = 1024
+SAT_2 = 2048
+SAT_4 = 4096
+SAT_8 = 8192
+SAT_16 = 16384
+SAT_32 = 32768
+SAT_64 = 65535
+SAT_128 = 65535
+SAT_256 = 65535
 
 # TCS34725 Gain Configuration
 TCS34725_REG_CONTROL_AGAIN_1 = 0x00  # 1x Gain
@@ -156,16 +177,24 @@ def num_gain(current_gain):
 def num_acquisition_time(current_acquisition_time):
     acquisition_time = 0
 
-    if current_acquisition_time == TCS34725_REG_ATIME_2_4:
-        acquisition_time = 2.4
-    elif current_acquisition_time == TCS34725_REG_ATIME_9_6:
-        acquisition_time = 9.6
-    elif current_acquisition_time == TCS34725_REG_ATIME_38_4:
-        acquisition_time = 38.4
-    elif current_acquisition_time == TCS34725_REG_ATIME_153_6:
-        acquisition_time = 153.6
-    elif current_acquisition_time == TCS34725_REG_ATIME_614_4:
-        acquisition_time = 614.4
+    if current_acquisition_time == TCS34725_REG_TIME_1:
+        acquisition_time = TIME_1
+    elif current_acquisition_time == TCS34725_REG_TIME_2:
+        acquisition_time = TIME_2
+    elif current_acquisition_time == TCS34725_REG_TIME_4:
+        acquisition_time = TIME_4
+    elif current_acquisition_time == TCS34725_REG_TIME_8:
+        acquisition_time = TIME_8
+    elif current_acquisition_time == TCS34725_REG_TIME_16:
+        acquisition_time = TIME_16        
+    elif current_acquisition_time == TCS34725_REG_TIME_32:
+        acquisition_time = TIME_32
+    elif current_acquisition_time == TCS34725_REG_TIME_64:
+        acquisition_time = TIME_64
+    elif current_acquisition_time == TCS34725_REG_TIME_128:
+        acquisition_time = TIME_128
+    elif current_acquisition_time == TCS34725_REG_TIME_256:
+        acquisition_time = TIME_266  
 
     return acquisition_time
 
@@ -233,10 +262,27 @@ def write_data(writer, sensor, year, month, day, hour, min, sec, lat, lon, alt, 
 
 
 # Check if the data is over exposed, under exposed or correctly exposed and return the corresponding tail
-def get_tail(red, green, blue, clear):
+def get_tail(red, green, blue, clear, current_acquisition_time):
     tail = "--"
-
-    if (red >= 40000 or green >= 40000 or blue >= 40000 or clear >= 40000) or (red == green and red == blue and red > 100):
+    if current_acquisition_time == TCS34725_REG_TIME_256:
+        sat = SAT_256
+    elif current_acquisition_time == TCS34725_REG_TIME_128:
+        sat = SAT_128
+    elif current_acquisition_time == TCS34725_REG_TIME_64:
+        sat = SAT_64
+    elif current_acquisition_time == TCS34725_REG_TIME_32:
+        sat = SAT_32
+    elif current_acquisition_time == TCS34725_REG_TIME_16:
+        sat = SAT_16
+    elif current_acquisition_time == TCS34725_REG_TIME_8:
+        sat = SAT_8
+    elif current_acquisition_time == TCS34725_REG_TIME_4:
+        sat = SAT_4            
+    elif current_acquisition_time == TCS34725_REG_TIME_2:
+        sat = SAT_2  
+    else:
+        sat = SAT_1  
+    if (red >= sat or green >= sat or blue >= sat or clear >= sat) or (red == green and red == blue and red > 100):
         tail = "OE"
     elif (red+green+blue > 2*clear) or (red+green+blue < 0.7*clear):
         tail = "ER"
@@ -252,42 +298,67 @@ def get_tail(red, green, blue, clear):
 
 def correction(red, green, blue, clear, current_gain, current_acquisition_time, current_waiting_time):
 
-    if ((red >= 40000 or green >= 40000 or blue >= 40000 or clear >= 40000) or (red == green and red == blue and red > 100) or (red+green+blue > 2*clear) or (red+green+blue < 0.7*clear)):
+    if current_acquisition_time == TCS34725_REG_TIME_256:
+        sat = SAT_256
+    elif current_acquisition_time == TCS34725_REG_TIME_128:
+        sat = SAT_128
+    elif current_acquisition_time == TCS34725_REG_TIME_64:
+        sat = SAT_64
+    elif current_acquisition_time == TCS34725_REG_TIME_32:
+        sat = SAT_32
+    elif current_acquisition_time == TCS34725_REG_TIME_16:
+        sat = SAT_16
+    elif current_acquisition_time == TCS34725_REG_TIME_8:
+        sat = SAT_8
+    elif current_acquisition_time == TCS34725_REG_TIME_4:
+        sat = SAT_4            
+    elif current_acquisition_time == TCS34725_REG_TIME_2:
+        sat = SAT_2  
+    else:
+        sat = SAT_1  
+    if ((red >= sat or green >= sat or blue >= sat or clear >= sat) or (red == green and red == blue and red > 100) or (red+green+blue > 2*clear) or (red+green+blue < 0.7*clear)):
         print("ERROR - SENSOR SATURATION : Trying to correct the settings...")
-        if current_acquisition_time == TCS34725_REG_ATIME_614_4:
-            current_acquisition_time = TCS34725_REG_ATIME_153_6
-        elif current_acquisition_time == TCS34725_REG_ATIME_153_6:
-            current_acquisition_time = TCS34725_REG_ATIME_38_4
-        elif current_acquisition_time == TCS34725_REG_ATIME_38_4:
-            current_acquisition_time = TCS34725_REG_ATIME_9_6
-        elif current_acquisition_time == TCS34725_REG_ATIME_9_6:
-            current_acquisition_time = TCS34725_REG_ATIME_2_4
+        if current_acquisition_time == TCS34725_REG_TIME_256:
+            current_acquisition_time = TCS34725_REG_TIME_128
+        elif current_acquisition_time == TCS34725_REG_TIME_128:
+            current_acquisition_time = TCS34725_REG_TIME_64
+        elif current_acquisition_time == TCS34725_REG_TIME_64:
+            current_acquisition_time = TCS34725_REG_TIME_32
+        elif current_acquisition_time == TCS34725_REG_TIME_32:
+            current_acquisition_time = TCS34725_REG_TIME_16
+        elif current_acquisition_time == TCS34725_REG_TIME_16:
+            current_acquisition_time = TCS34725_REG_TIME_8
+        elif current_acquisition_time == TCS34725_REG_TIME_8:
+            current_acquisition_time = TCS34725_REG_TIME_4
+        elif current_acquisition_time == TCS34725_REG_TIME_4:
+            current_acquisition_time = TCS34725_REG_TIME_2            
+        elif current_acquisition_time == TCS34725_REG_TIME_2:
+            current_acquisition_time = TCS34725_REG_TIME_1               
         else:
             print("There is just too much light...... :( ")
     elif clear <= 299:
         print("ERROR = SENSOR UNDEREXPOSED : Trying to correct the settings...")
-        if current_acquisition_time == TCS34725_REG_ATIME_2_4 :
-            current_acquisition_time = TCS34725_REG_ATIME_9_6
-        elif current_acquisition_time == TCS34725_REG_ATIME_9_6 :
-            current_acquisition_time = TCS34725_REG_ATIME_38_4
-        elif current_acquisition_time == TCS34725_REG_ATIME_38_4 :
-            current_acquisition_time = TCS34725_REG_ATIME_153_6
-        elif current_acquisition_time == TCS34725_REG_ATIME_153_6:
-            current_acquisition_time = TCS34725_REG_ATIME_614_4
+        if current_acquisition_time == TCS34725_REG_TIME_1:
+            current_acquisition_time = TCS34725_REG_TIME_2
+        elif current_acquisition_time == TCS34725_REG_TIME_2:
+            current_acquisition_time = TCS34725_REG_TIME_4
+        elif current_acquisition_time == TCS34725_REG_TIME_4:
+            current_acquisition_time = TCS34725_REG_TIME_8
+        elif current_acquisition_time == TCS34725_REG_TIME_8:
+            current_acquisition_time = TCS34725_REG_TIME_16
+        elif current_acquisition_time == TCS34725_REG_TIME_16:
+            current_acquisition_time = TCS34725_REG_TIME_32
+        elif current_acquisition_time == TCS34725_REG_TIME_32:
+            current_acquisition_time = TCS34725_REG_TIME_64
+        elif current_acquisition_time == TCS34725_REG_TIME_64:
+            current_acquisition_time = TCS34725_REG_TIME_128            
+        elif current_acquisition_time == TCS34725_REG_TIME_128:
+            current_acquisition_time = TCS34725_REG_TIME_256               
         else:
             print("There is just not enough light...... :( ")
     else:
         print("\b\bData have been correctly gathered")
-    if current_acquisition_time == TCS34725_REG_ATIME_2_4:
-        current_waiting_time = TCS34725_REG_WTIME_4_8
-    elif current_acquisition_time == TCS34725_REG_ATIME_9_6:
-        current_waiting_time = TCS34725_REG_WTIME_12
-    elif current_acquisition_time == TCS34725_REG_ATIME_38_4:
-        current_waiting_time = TCS34725_REG_WTIME_40_8
-    elif current_acquisition_time == TCS34725_REG_ATIME_153_6:
-        current_waiting_time = TCS34725_REG_WTIME_156
-    else:
-        current_waiting_time = TCS34725_REG_WTIME_614_4
+    current_waiting_time = current_acquisition_time
     current_gain = TCS34725_REG_CONTROL_AGAIN_60
     return {'c_g': current_gain, 'c_at': current_acquisition_time, 'c_wt': current_waiting_time}
 
@@ -595,7 +666,7 @@ while end == 0:
             acqt = num_acquisition_time(ATS[a])
             lux = clux(lum['l'] , gain, acqt)
             temp = colour_temperature(lum['r'], lum['g'], lum['b'], lum['c'])
-            tail[a] = get_tail(lum['r'], lum['g'], lum['b'], lum['c'])
+            tail[a] = get_tail(lum['r'], lum['g'], lum['b'], lum['c'], ATS[a])
             print("Corrected lux=", lux)
             # Used for gps
             today = str(datetime.now())
