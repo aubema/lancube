50a43a1c1dada6061ab193764734d1a37981882e
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-12-15 14:31:44 -0500

    test gain

--- lan3v2.py
+++ lan3v2.py
@@ -86,9 +86,15 @@ def gain_selection(sensor, G):
 def readluminance(sensor):
     """Read data back from TCS34725_REG_CDATAL(0x94), 8 bytes, with TCS34725_COMMAND_BIT, (0x80)
     cData LSB, cData MSB, Red LSB, Red MSB, Green LSB, Green MSB, Blue LSB, Blue MSB"""
-    data = sensor.read_i2c_block_data(TCS34725_DEFAULT_ADDRESS,
+    data[0] = 0 
+    data[2] = 0
+    data[4] = 0 
+    data[6] = 0
+    ntry = 1
+    while (data[0] != 0) and (data[2] != 0) and (data[4] != 0) and (data[6] != 0) and (ntry < 99):    
+        data = sensor.read_i2c_block_data(TCS34725_DEFAULT_ADDRESS,
                                       TCS34725_REG_CDATAL | TCS34725_COMMAND_BIT, 8)
-
+        ntry += 1
     # Convert the data
     cData = data[1] * 256 + data[0]
     red = data[3] * 256 + data[2]
@@ -591,7 +597,7 @@ while end == 0:
             gain_selection(capteur[a], GS[a])
             lum = readluminance(capteur[a])
             ntry = 1
-            while (( lum['r']+lum['g']+lum['b'] > 1.3*lum['c']) or (lum['r']+lum['g']+lum['b'] < 0.7*lum['c']) or (lum['l'] < 0) or (lum['c'] == 1024)) and ( ntry < 999):
+            while (( lum['r']+lum['g']+lum['b'] > 1.3*lum['c']) or (lum['r']+lum['g']+lum['b'] < 0.7*lum['c']) or (lum['l'] < 0)) and ( ntry < 999):
                lum = readluminance(capteur[a])
                ntry += 1
             time_str = get_time()
