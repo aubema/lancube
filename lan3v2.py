401f8aba3dbcca7a3a053494f8519d0f3640cdee
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-12-15 16:21:01 -0500

    test gain

--- lan3v2.py
+++ lan3v2.py
@@ -254,6 +254,8 @@ def correction(red, green, blue, clear, current_gain, current_acquisition_time, 
 
     if (red >= 40000 or green >= 40000 or blue >= 40000 or clear >= 40000) or (red == green and red == blue and red > 100):
         print("ERROR - SENSOR SATURATION : Trying to correct the settings...")
+        if (current_gain == TCS34725_REG_CONTROL_AGAIN_60) and (current_acquisition_time == TCS34725_REG_ATIME_2_4):
+            current_gain = TCS34725_REG_CONTROL_AGAIN_16
         if current_acquisition_time == TCS34725_REG_ATIME_2_4 and current_waiting_time == TCS34725_REG_WTIME_4_8 and current_gain == TCS34725_REG_CONTROL_AGAIN_1:
             print("There is just too much light...... :( ")
     elif (red+green+blue > 1.3*clear) or (red+green+blue < 0.7*clear):
