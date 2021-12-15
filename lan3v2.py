7e024e1f40b7788a4cc531893b343d336b547bcf
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-12-15 14:38:23 -0500

    test gain

--- lan3v2.py
+++ lan3v2.py
@@ -88,11 +88,12 @@ def readluminance(sensor):
     cData LSB, cData MSB, Red LSB, Red MSB, Green LSB, Green MSB, Blue LSB, Blue MSB"""
     data = sensor.read_i2c_block_data(TCS34725_DEFAULT_ADDRESS,
                                       TCS34725_REG_CDATAL | TCS34725_COMMAND_BIT, 8)
-    ntry = 1
-    while (data[0] != 0) and (data[2] != 0) and (data[4] != 0) and (data[6] != 0) and (ntry < 99):    
+    ntry2 = 1
+    while (data[0] != 0) and (data[2] != 0) and (data[4] != 0) and (data[6] != 0) and (ntry2 < 99):    
         data = sensor.read_i2c_block_data(TCS34725_DEFAULT_ADDRESS,
                                       TCS34725_REG_CDATAL | TCS34725_COMMAND_BIT, 8)
-        ntry += 1
+        print("ntry2=", ntry2)                              
+        ntry2 += 1
     # Convert the data
     cData = data[1] * 256 + data[0]
     red = data[3] * 256 + data[2]
