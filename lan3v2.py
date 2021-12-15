4298bf1933efa6c32cb6d06a8f2bc0a29ef05b93
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2021-12-15 17:17:52 -0500

    test gain

--- lan3v2.py
+++ lan3v2.py
@@ -238,7 +238,7 @@ def get_tail(red, green, blue, clear):
 
     if (red >= 40000 or green >= 40000 or blue >= 40000 or clear >= 40000) or (red == green and red == blue and red > 100):
         tail = "OE"
-    elif (red+green+blue > 1.3*clear) or (red+green+blue < 0.7*clear):
+    elif (red+green+blue > 1.5*clear) or (red+green+blue < 0.7*clear):
         tail = "ER"
     elif clear <= 99:
         tail = "UE"
@@ -252,7 +252,7 @@ def get_tail(red, green, blue, clear):
 
 def correction(red, green, blue, clear, current_gain, current_acquisition_time, current_waiting_time):
 
-    if ((red >= 40000 or green >= 40000 or blue >= 40000 or clear >= 40000) or (red == green and red == blue and red > 100) or (red+green+blue > 1.3*clear) or (red+green+blue < 0.7*clear)):
+    if ((red >= 40000 or green >= 40000 or blue >= 40000 or clear >= 40000) or (red == green and red == blue and red > 100) or (red+green+blue > 1.5*clear) or (red+green+blue < 0.7*clear)):
         print("ERROR - SENSOR SATURATION : Trying to correct the settings...")
         if current_acquisition_time == TCS34725_REG_ATIME_614_4:
             current_acquisition_time = TCS34725_REG_ATIME_153_6
