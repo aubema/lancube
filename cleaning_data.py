4f836da41df896d23a91400c1c3acd6cdf6a5036
Julien-Pierre Houle <jphoule29@hotmail.com>
2022-05-30 13:12:15 -0400

    Update cleaning_data.py

--- cleaning_data.py
+++ cleaning_data.py
@@ -2,97 +2,89 @@
 
 # Cleaning the data measured by the lancube
 # Author : Julien-Pierre Houle
-# Last update : August 2021
 
 
-import os
 import numpy as np
 import pandas as pd
 import osmnx as ox
-import progressbar
-from datetime import datetime
 from pyproj import CRS, Transformer
-import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
-from scipy.ndimage.interpolation import shift
-from cleaning_data import *
-
-
 
 def delete_sequence(df, index_to_delete):
     """ Function to return the full sequence of sensors to delete when a value is filtered. """
 
-    print('Filtering data...')
-    sequence_to_delete = []
-    matching_sensor = df["Sensor"].iloc[index_to_delete].values.tolist()
-    sensor_number = [int(s[-1]) for s in matching_sensor] # nb associate to the sensor (between 1-5)
-
-    bar = progressbar.ProgressBar(maxval=len(index_to_delete)).start()
+    print('Filtering data.')
+    seq_to_delete = []
+    sensor = df["Sensor"].iloc[index_to_delete].values.tolist()    
+   
     for idx, index_error in enumerate(index_to_delete):
-        sensor = sensor_number[idx] # sensor number
-        diff_sup = 5-sensor
+                
+        if sensor[idx] == 'S1':
+            seq_to_delete += list(range(index_error, index_error+3))
+            
+        if sensor[idx] == 'S3':
+            seq_to_delete += [index_error-1, index_error, index_error+1]
+            
+        if sensor[idx] == 'S5':
+            seq_to_delete += list(range(index_error, index_error-3, -1))
+        
+    seq_to_delete = list(set(seq_to_delete))
 
-        if index_error not in sequence_to_delete:
-            sequence_sup = list(range(index_error, index_error+diff_sup+1))
-            sequence_inf = list(reversed((range(index_error-sensor+1, index_error))))
-
-            sequence_to_delete += (sequence_sup+sequence_inf)
-            bar.update(idx)
-
-    return sequence_to_delete
-
+    return seq_to_delete
 
 
 
 
 def cleaning_data(df):
     """ Cleaning the data measured by the lancube. """
-
-
+    
+    # PATH_DATAS = "Data"
+    # df = pd.read_csv(f"{PATH_DATAS}/Corr_2021-10-28-temp.csv", sep=',', on_bad_lines='skip')
+    # df['Value'] = df['lux']
+    
+    
     df['Time'] = pd.to_datetime(df[['Year','Month','Day','Hour','Minute','Second']],
-                                format="Y-%m-%d %H-%M-%S")
+                                                         format="Y-%m-%d %H-%M-%S")
     df = df.drop(columns=['Year','Month','Day','Hour','Minute', 'Second'])
-    df = df.dropna(how='all') # delete if all values are NaN
-    df = df.dropna(subset=['Sensor'])
-    df = df.reset_index(drop=True)
+    
+    df = df.drop( df[ (df.Sensor=='S2') | (df.Sensor == 'S4')].index ) # delete S2 & S4 sensor
+    df = df.dropna(how='all')  # delete row if all values are NaN
+    df = df.reset_index(drop=True) # reset a normal index
 
 
-    # Verify the order sensor
-    index = df['Sensor'].index.values
+    # Verify sensor order
+    index = df.index.values
     sensors_nb = np.array([int(s[-1]) for s in df['Sensor'].tolist()]) # value between 1-5
-    diff_prev = np.diff(sensors_nb, prepend=0)
-    idx_error = index[(diff_prev != 1) & (diff_prev != -4)]
-    idx_error = np.append(idx_error, [0, df.index[-1]]) # check first and last sensors
+    diff_prev = np.diff(sensors_nb, prepend=0) # diff with previous values
+    idx_error = index[(diff_prev != 2) & (diff_prev != -4)] # error if diff not 2 or -4
+    idx_error = np.append(idx_error, df.index[-1]) # check last sensors
+    
 
-
-    # Clean the order sensor
+    # Cleaning the sensor order
     index_to_delete = []
     for i in range(len(idx_error)):
-
+        
         idx = idx_error[i]
-        sensor = df['Sensor'].iloc[idx]
-
-        if sensor != 'S5' and sensor != 'S1':
-            index_to_delete.append(idx)
+        index_to_delete.append(idx)
+        
 
         # Low sensors
-        while (sensor != 'S5') and (idx > 0):
+        prev_sensor = 0
+        while (prev_sensor != 'S5') and (idx > 0):
             idx -= 1
-            sensor = df['Sensor'].iloc[idx]
-            if sensor != 'S5':
-                # print(idx, sensor)
+            prev_sensor = df['Sensor'].iloc[idx]
+            
+            if prev_sensor != 'S5':
                 index_to_delete.append(idx)
 
-
         idx = idx_error[i]
-        sensor = df['Sensor'].iloc[idx]
 
         # Top sensors
-        while (sensor != 'S1') and (idx < len(df)-1):
+        next_sensor = 0
+        while (next_sensor != 'S1') and (idx < len(df)-1):
             idx += 1
-            sensor = df['Sensor'].iloc[idx]
-            if sensor != 'S1':
-                # print(idx, sensor)
+            next_sensor = df['Sensor'].iloc[idx]
+                        
+            if next_sensor != 'S1':
                 index_to_delete.append(idx)
 
     index_to_delete.sort()
@@ -113,7 +105,8 @@ def cleaning_data(df):
     mask_lon = (df['Longitude'] > avg_lon+2) & (df['Longitude'] < avg_lon-2)
     index_to_delete += df[mask_lat & mask_lon].index.tolist()
 
-    index_to_delete += df['Value'][df['Value'].abs() > 10].index.tolist()
+
+    # index_to_delete += df['Value'][df['Value'].abs() > 10].index.tolist()
 
 
 
@@ -123,8 +116,14 @@ def cleaning_data(df):
     dy = np.pi/180 * 6373000 * df['Latitude'].diff()
     dx = np.pi/180 * 6373000 * df['Longitude'].diff() * np.sin(df['Latitude'])
     D = np.sqrt(dx**2 + dy**2)
-    index_to_delete += D[D > 5].index.tolist()
-
+    
+    # 20m correspond 120 km/h (condition tres noire)
+    index_to_delete += D[D > 20].index.tolist()
+    
+    
+    # Check for negative lux to delete
+    mask_lux = (df['lux'] < 0)
+    index_to_delete += df[mask_lux].index.tolist()
 
 
     # 3. Filter Over Expose S1 and abberant values
@@ -186,12 +185,46 @@ def cleaning_data(df):
     X, Y = transformer.transform(df['Longitude'], df['Latitude'])
 
     dX, dY = np.diff(X), np.diff(Y)
-    dDist = np.sqrt((dX**2) + (dY**2)) # pytagore
+    dDist = np.sqrt((dX**2) + (dY**2)) # pythagore
     traveled_distance = np.cumsum(dDist) # cumulative sum of the distance
-    df["Traveled Distance"] = np.insert(traveled_distance, 0, np.nan).tolist() # add nan
+    df["distance"] = np.insert(traveled_distance, 0, np.nan).tolist() # add nan first elem
 
     df1 = df[df['Sensor'] == 'S1']
     df3 = df[df['Sensor'] == 'S3']
     df5 = df[df['Sensor'] == 'S5']
+    
+    # Rename columns
+    df1.rename(columns = {'Latitude':'lat', 'Longitude':'lon'}, inplace=True)
+    df3.rename(columns = {'Latitude':'lat', 'Longitude':'lon'}, inplace=True)
+    df5.rename(columns = {'Latitude':'lat', 'Longitude':'lon'}, inplace=True)
+    
+    # Reset index
+    df1 = df1.reset_index()
+    df3 = df3.reset_index()
+    df5 = df5.reset_index()
+
 
     return df1, df3, df5
+
+
+
+# PATH_DATAS = "Data"
+# df = pd.read_csv(f"{PATH_DATAS}/Corr_2021-10-28-temp.csv", sep=',', on_bad_lines='skip')
+# df['Value'] = df['lux']
+
+
+# df = pd.read_csv('Data/Mtl_june_2021/2021-06-07.csv')
+# df.rename(columns={'Acquisition time (ms)': 'AcquisitionTime'}, inplace=True)
+# df['Value'] = df['Clear'] / df['Gain'] / df['AcquisitionTime']
+
+# PATH_DATAS = "Data/Mtl_2"
+# df = pd.read_csv(f"{PATH_DATAS}/Corr_2021-06-07.csv", sep=',', on_bad_lines='skip')
+# df['Value'] = df['Clear'] / df['Gain'] / df['AcquisitionTime']
+
+
+# df1, df3, df5 = cleaning_data(df)
+
+
+# import matplotlib.pyplot as plt
+# plt.plot(df1['Traveled Distance'], df1['Value'])
+# plt.show()
