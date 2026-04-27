c8c4ccafb17912231eb28b9ca97f018a5a50e791
aubema <aubema@gmail.com>
2026-04-27 17:00:50 -0400

    adding gps reset designed by Alex Mavrovic

--- lan3v3.py
+++ lan3v3.py
@@ -687,7 +687,7 @@ def reset_gps_usb(port="/dev/ttyACM0"):
 
 
 # initialisation ============================
-reset_gps_usb(port=SERIAL_PORT)
+reset_gps_usb(port=/dev/ttyACM0)
 time.sleep(2)               # laisse le port se stabiliser
 
 # LED
